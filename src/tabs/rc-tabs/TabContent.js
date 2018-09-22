f4fbf65d0105ebe6ae680f29830cce5cc6da94fc
chenlin <jiay0u@yahoo.com>
2018-09-22 14:22:29 +0800

    动态调整content高度

---
+++ src/tabs/rc-tabs/TabContent.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import {
+  getTransformByIndex,
+  getActiveIndex,
+  getTransformPropValue,
+  getMarginStyle,
+} from './utils';
+
+export default class TabContent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contentH: document.body.clientHeight - 104
+    }
+  }
+  getTabPanes() {
+    const props = this.props;
+    const activeKey = props.activeKey;
+    const children = props.children;
+    const newChildren = [];
+
+    React.Children.forEach(children, (child) => {
+      if (!child) {
+        return;
+      }
+      const key = child.key;
+      const active = activeKey === key;
+      newChildren.push(React.cloneElement(child, {
+        active,
+        destroyInactiveTabPane: props.destroyInactiveTabPane,
+        rootPrefixCls: props.prefixCls,
+      }));
+    });
+
+    return newChildren;
+  }
+
+  componentWillMount() {
+    const onresize = () => {
+      this.setState({
+        contentH: document.body.clientHeight - 104
+      })
+    }
+    window.onresize = onresize
+  }
+
+  render() {
+    const { props } = this;
+    const {
+      prefixCls, children, activeKey,
+      tabBarPosition, animated, animatedWithMargin,
+    } = props;
+    let { style } = props;
+    const classes = classnames({
+      [`${prefixCls}-content`]: true,
+      [animated ?
+        `${prefixCls}-content-animated` :
+        `${prefixCls}-content-no-animated`]: true,
+    });
+    if (animated) {
+      const activeIndex = getActiveIndex(children, activeKey);
+      if (activeIndex !== -1) {
+        const animatedStyle = animatedWithMargin ?
+                getMarginStyle(activeIndex, tabBarPosition) :
+                getTransformPropValue(getTransformByIndex(activeIndex, tabBarPosition));
+        style = {
+          ...style,
+          ...animatedStyle,
+        };
+      } else {
+        style = {
+          ...style,
+          display: 'none',
+        };
+      }
+    }
+    style = {height: this.state.contentH, ...style,}
+    return (
+      <div
+        className={classes}
+        style={style}
+      >
+        {this.getTabPanes()}
+      </div>
+    );
+  }
+}
+
+TabContent.propTypes = {
+  animated: PropTypes.bool,
+  animatedWithMargin: PropTypes.bool,
+  prefixCls: PropTypes.string,
+  children: PropTypes.node,
+  activeKey: PropTypes.string,
+  style: PropTypes.any,
+  tabBarPosition: PropTypes.string,
+};
+
+TabContent.defaultProps = {
+  animated: true,
+};
---
+++ src/tabs/rc-tabs/TabPane.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { getDataAttr } from './utils';
+
+export default class TabPane extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      paneH: document.body.clientHeight - 104
+    }
+  }
+  componentWillMount() {
+    const onresize = () => {
+      this.setState({
+        paneH: document.body.clientHeight - 104
+      })
+    }
+    window.onresize = onresize
+  }
+  render() {
+    const {
+      id, className, destroyInactiveTabPane, active, forceRender,
+      rootPrefixCls, style, children, placeholder, ...restProps,
+    } = this.props;
+    this._isActived = this._isActived || active;
+    const prefixCls = `${rootPrefixCls}-tabpane`;
+    const cls = classnames({
+      [prefixCls]: 1,
+      [`${prefixCls}-inactive`]: !active,
+      [`${prefixCls}-active`]: active,
+      [className]: className,
+    });
+    const isRender = destroyInactiveTabPane ? active : this._isActived;
+    return (
+      <div
+        style={{height: this.state.paneH, ...style,}}
+        role="tabpanel"
+        aria-hidden={active ? 'false' : 'true'}
+        className={cls}
+        id={id}
+        {...getDataAttr(restProps)}
+      >
+        {isRender || forceRender ? children : placeholder}
+      </div>
+    );
+  }
+}
+
+TabPane.propTypes = {
+  className: PropTypes.string,
+  active: PropTypes.bool,
+  style: PropTypes.any,
+  destroyInactiveTabPane: PropTypes.bool,
+  forceRender: PropTypes.bool,
+  placeholder: PropTypes.node,
+  rootPrefixCls: PropTypes.string,
+  children: PropTypes.node,
+  id: PropTypes.string,
+};
+
+TabPane.defaultProps = {
+  placeholder: null,
+};
